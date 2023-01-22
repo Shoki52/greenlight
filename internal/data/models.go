aec5bd97fbcc312e1157ff59650b2dc137a38d84
Shoki52 <halhabai.shokan@gmail.com>
2023-01-22 23:03:51 +0600

    Assignment3

---
+++ internal/data/models.go
@@ -0,0 +1,23 @@
+package data
+
+import (
+	"errors"
+	"github.com/jackc/pgx/v5/pgxpool"
+)
+
+var (
+	ErrRecordNotFound = errors.New("record not found")
+	ErrEditConflict   = errors.New("edit conflict")
+)
+
+type Models struct {
+	Movies MovieModel
+	Users  UserModel // Add a new Users field.
+}
+
+func NewModels(db *pgxpool.Pool) Models {
+	return Models{
+		Movies: MovieModel{DB: db},
+		Users:  UserModel{DB: db},
+	}
+}
